cb106c4b080d58430ae168973ffd2672fc9cf0cd
kasutaja <stas@tlu.ee>
2019-03-16 16:28:50 +0200

    Added: Transparent wall

--- Therapo_v1/Assets/InteractScript.cs
+++ Therapo_v1/Assets/InteractScript.cs
@@ -15,23 +15,24 @@ public class InteractScript : MonoBehaviour
     [HideInInspector] public GameObject TextPrefabInstanceCopy; // A copy of the text prefab to prevent data corruption
     [HideInInspector] public bool TextActive;
 
+    public GameObject TransparentWall;
+
     public float interactDistance = 5f;
 
     private void Start()
     {
         TextPrefabInstance.gameObject.SetActive(false);
-
+        TextActive = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //  TextPrefab.GetComponent<Renderer>().enabled = false;
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, interactDistance))
         {
-            Debug.Log("BLA " + hit.collider.gameObject);
+            Debug.Log(hit.collider.gameObject);
 
             if (hit.collider.tag == "Door")
             {
@@ -39,25 +40,18 @@ public class InteractScript : MonoBehaviour
                 InReach = true;
 
                 // Display the UI element when the player is in reach of the door
-                if (TextActive == false)
+                if (TextActive)
                 {
                     TextPrefabInstance.gameObject.SetActive(true);
-                    //   TextPrefabInstance = Instantiate(TextPrefab);
-                    //     TextPrefabInstance.transform.localScale = new Vector3(0, 0, -312);
-                    //     Destroy(TextPrefab);
-                    TextActive = true;
-                    //  TextPrefabInstance.transform.SetParent(transform, true); // Make the player the parent object of the text element
                 }
 
                 if (OVRInput.Get(OVRInput.Button.Two))
                 {
                     hit.collider.transform.parent.GetComponent<DoorScript>().ChangeDoorState();
                     Destroy(hit.collider.GetComponent<BoxCollider>());
-                    if (TextActive)
-                    {
-                        TextPrefabInstance.gameObject.SetActive(false);
-                        TextActive = false;
-                    }
+
+                    TextPrefabInstance.gameObject.SetActive(false);
+                    TextActive = false;
                 }
             }
         }
@@ -65,15 +59,21 @@ public class InteractScript : MonoBehaviour
         else
         {
             InReach = false;
-
+            TextPrefabInstance.gameObject.SetActive(false);
             // Destroy the UI element when Player is no longer in reach of the door
-            if (TextActive)
-            {
-                TextPrefabInstance.gameObject.SetActive(false);
-                TextActive = false;
-            }
+            /*    if (TextActive)
+                {
+                    TextPrefabInstance.gameObject.SetActive(false);
+                    TextActive = false;
+                }*/
         }
 
-
+        if (TransparentWall.activeSelf)
+        {
+            if (OVRInput.Get(OVRInput.Button.Three))
+            {
+                TransparentWall.gameObject.SetActive(false);
+            }
+        }
     }
 }
